7fef3ff11fd23b296d8377a7a81fb7001a076b27
Edilaine Dias Lima <layne.dl@gmail.com>
2023-02-24 09:23:10 -0300

    first commit

---
+++ src/conectandocommeugithub/testeGit.java
@@ -0,0 +1,12 @@
+package conectandocommeugithub;
+
+public class testeGit {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Testando meu Git");
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author layne
+ *
+ */
+module conectandocommeugithub {
+}
